c7c5521ad30bf718aff69dfec8c0b1f2d5e6b212
NOTAPIYC <andreas_postkasten@gmx.net>
2019-12-02 08:51:37 +0100

    MouseOver and Pressed button state colors customized via an additional custom renderer for the UWP platform.

---
+++ EvenShare/Misc/FabButton.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace EvenShare
+{
+    public class FabButton : Button
+    {
+        public FabButton() : base()
+        {
+            // This is just a target for the custom renderer
+        }
+    }
+}
